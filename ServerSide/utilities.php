20c3f358abd3a0ea125a8b3c4bc625f5f00ce0eb
thm0321 <42718434+thm0321@users.noreply.github.com>
2019-10-14 16:24:24 -0400

    Add files via upload

---
+++ ServerSide/utilities.php
@@ -0,0 +1,47 @@
+<?php
+    //Uses Admin  table 
+    //This function adds an admin, someone who has the ability to update, delete and add to the modules table
+    function addAdmin(){
+
+    }
+    //This function gets an admin from the database
+    function getAdmin(){
+
+    }
+    //This function removes an Admin from the database
+    function removeAdmin(){
+        
+    }
+    //Uses the Modules table
+    //This function adds a unity to scene (entry) to the database
+    function addScene(){
+
+    }
+    //This function updates and existing Unity scene in the database
+    function updateScene(){
+
+    }
+    //This function deletes a unity scene (entry) from the database
+    function removeScene(){
+
+    }
+    //This function gets a unity scene (entry) from the database 
+    function getScene(){
+
+    }
+    //This function retrieves all the entries from the Modules table
+    function getAllScenes(){
+
+    }
+    //Ueses the History table
+    //This functions adds the packages of modules that were sent to prospective students
+    function addHistory(){
+
+    }
+    //This function 
+    
+
+
+
+
+?>

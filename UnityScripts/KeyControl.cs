f459ceb6cc65ce81c3c8e71c7fecd552e2725e63
isbriggs <42577950+isbriggs@users.noreply.github.com>
2019-10-23 16:54:07 -0400

    Add files via upload

---
+++ UnityScripts/KeyControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyControl : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    //public float speed = .15F;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float rotHoriz = Input.GetAxis("Horizontal");
+        float rotVert  = -Input.GetAxis("Vertical");
+        transform.Rotate(rotVert, 0f, 0f, Space.Self);
+        transform.Rotate(0f, rotHoriz, 0f, Space.World);
+    }
+}
